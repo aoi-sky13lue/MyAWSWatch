3b28698283c0947ea33224de1af4a1e3ac6d4234
hal-art <halcorder@gmail.com>
2023-12-21 00:44:39 +0900

    ターゲットsdk修正した

--- src/MyAWSWatch/app/build.gradle.kts
+++ src/MyAWSWatch/app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.hal.myawswatch"
         minSdk = 31
-        targetSdk = 33
+        targetSdk = 31
         versionCode = 1
         versionName = "1.0"
 
@@ -59,6 +59,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.core:core-splashscreen:1.0.0-beta02")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
