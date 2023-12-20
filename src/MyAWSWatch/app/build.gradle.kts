4a9869e01762601111cdbbdd303d6988e0dfce02
hal-art <halcorder@gmail.com>
2023-12-21 02:17:39 +0900

    kotlin coroutine追加

--- src/MyAWSWatch/app/build.gradle.kts
+++ src/MyAWSWatch/app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     val lottieVersion = "6.2.0"
+    val coroutineVersion = "1.3.9"
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
@@ -63,6 +64,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("com.airbnb.android:lottie-compose:$lottieVersion")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutineVersion")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
