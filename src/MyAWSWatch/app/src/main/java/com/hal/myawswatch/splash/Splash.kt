ab186f37cec7f65207774403b2a1b642a5361bfc
hal-art <halcorder@gmail.com>
2023-12-21 01:47:52 +0900

    スプラッシュ対応追加

--- src/MyAWSWatch/app/src/main/java/com/hal/myawswatch/splash/Splash.kt
+++ src/MyAWSWatch/app/src/main/java/com/hal/myawswatch/splash/Splash.kt
@@ -6,15 +6,26 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.hal.myawswatch.R
 import com.hal.myawswatch.splash.ui.theme.MyAWSWatchTheme
+import com.hal.myawswatch.utils.SysUiUtils
 
-class splash : ComponentActivity() {
+/**
+ * Splashクラス
+ * */
+class Splash : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        SysUiUtils().hideSystemBars(window)
         setContent {
             MyAWSWatchTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,25 +33,53 @@ class splash : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    DrawSplash()
                 }
             }
         }
     }
 }
 
+/**
+ * プレビュー(Light Theme)処理
+ *
+ * @author aoi
+ * */
+@Preview(name = "Light Theme", showBackground = true)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun LightThemePreview() {
+    MyAWSWatchTheme {
+        DrawSplash()
+    }
 }
 
-@Preview(showBackground = true)
+/**
+ * プレビュー(Dark Theme)処理
+ *
+ * @author aoi
+ * */
+@Preview(name = "Dark Theme", showBackground = false)
 @Composable
-fun GreetingPreview() {
+fun DarkThemePreview() {
     MyAWSWatchTheme {
-        Greeting("Android")
+        DrawSplash()
     }
+}
+
+/**
+ * Lottieを使用したスプラッシュスクリーン表示用のコンポーザブル関数。
+ * アニメーションは無限にループします。
+ * スプラッシュ完了後の処理は呼び出し元が担当します。
+ *
+ * @author aoi
+ */
+@Composable
+fun DrawSplash() {
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.app_launch_splash))
+    val progress by animateLottieCompositionAsState(composition, iterations = LottieConstants.IterateForever)
+
+    LottieAnimation(
+        composition = composition,
+        progress = { progress },
+    )
 }
