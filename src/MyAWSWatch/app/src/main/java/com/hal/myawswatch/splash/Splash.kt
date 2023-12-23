14c30e8ae177972e581de31279e51a8e6028e8ea
hal-art <halcorder@gmail.com>
2023-12-23 18:17:16 +0900

    起動時のスプラッシュカラーを統一

--- src/MyAWSWatch/app/src/main/java/com/hal/myawswatch/splash/Splash.kt
+++ src/MyAWSWatch/app/src/main/java/com/hal/myawswatch/splash/Splash.kt
@@ -5,11 +5,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
@@ -69,7 +69,7 @@ fun Draw(){
         // A surface container using the 'background' color from the theme
         Surface(
             modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background
+            color = Color.Black
         ) {
             DrawSplash()
         }
