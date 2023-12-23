8e73b0f776f8078a714f5d3b75908014a5870a9e
hal-art <halcorder@gmail.com>
2023-12-23 17:52:40 +0900

    ライトテーマとダークテーマを統一した

--- src/MyAWSWatch/app/src/main/java/com/hal/myawswatch/login/ui/theme/Color.kt
+++ src/MyAWSWatch/app/src/main/java/com/hal/myawswatch/login/ui/theme/Color.kt
@@ -2,37 +2,6 @@ package com.hal.myawswatch.login.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val md_theme_light_primary = Color(0xFF006685)
-val md_theme_light_onPrimary = Color(0xFFFFFFFF)
-val md_theme_light_primaryContainer = Color(0xFFBEE9FF)
-val md_theme_light_onPrimaryContainer = Color(0xFF001F2A)
-val md_theme_light_secondary = Color(0xFF4D616C)
-val md_theme_light_onSecondary = Color(0xFFFFFFFF)
-val md_theme_light_secondaryContainer = Color(0xFFD0E6F2)
-val md_theme_light_onSecondaryContainer = Color(0xFF081E27)
-val md_theme_light_tertiary = Color(0xFF5E5B7D)
-val md_theme_light_onTertiary = Color(0xFFFFFFFF)
-val md_theme_light_tertiaryContainer = Color(0xFFE4DFFF)
-val md_theme_light_onTertiaryContainer = Color(0xFF1A1836)
-val md_theme_light_error = Color(0xFFBA1A1A)
-val md_theme_light_errorContainer = Color(0xFFFFDAD6)
-val md_theme_light_onError = Color(0xFFFFFFFF)
-val md_theme_light_onErrorContainer = Color(0xFF410002)
-val md_theme_light_background = Color(0xFFFBFCFE)
-val md_theme_light_onBackground = Color(0xFF191C1E)
-val md_theme_light_surface = Color(0xFFFBFCFE)
-val md_theme_light_onSurface = Color(0xFF191C1E)
-val md_theme_light_surfaceVariant = Color(0xFFDCE4E9)
-val md_theme_light_onSurfaceVariant = Color(0xFF40484C)
-val md_theme_light_outline = Color(0xFF70787D)
-val md_theme_light_inverseOnSurface = Color(0xFFF0F1F3)
-val md_theme_light_inverseSurface = Color(0xFF2E3133)
-val md_theme_light_inversePrimary = Color(0xFF6BD3FF)
-val md_theme_light_shadow = Color(0xFF000000)
-val md_theme_light_surfaceTint = Color(0xFF006685)
-val md_theme_light_outlineVariant = Color(0xFFC0C8CD)
-val md_theme_light_scrim = Color(0xFF000000)
-
 val md_theme_dark_primary = Color(0xFF6BD3FF)
 val md_theme_dark_onPrimary = Color(0xFF003546)
 val md_theme_dark_primaryContainer = Color(0xFF004D65)
@@ -63,6 +32,37 @@ val md_theme_dark_shadow = Color(0xFF000000)
 val md_theme_dark_surfaceTint = Color(0xFF6BD3FF)
 val md_theme_dark_outlineVariant = Color(0xFF40484C)
 val md_theme_dark_scrim = Color(0xFF000000)
+
+val md_theme_light_primary = md_theme_dark_primary
+val md_theme_light_onPrimary = md_theme_dark_onPrimary
+val md_theme_light_primaryContainer = md_theme_dark_primaryContainer
+val md_theme_light_onPrimaryContainer = md_theme_dark_onPrimaryContainer
+val md_theme_light_secondary = md_theme_dark_secondary
+val md_theme_light_onSecondary = md_theme_dark_onSecondary
+val md_theme_light_secondaryContainer = md_theme_dark_secondaryContainer
+val md_theme_light_onSecondaryContainer = md_theme_dark_onSecondaryContainer
+val md_theme_light_tertiary = md_theme_dark_tertiary
+val md_theme_light_onTertiary = md_theme_dark_onTertiary
+val md_theme_light_tertiaryContainer = md_theme_dark_tertiaryContainer
+val md_theme_light_onTertiaryContainer = md_theme_dark_onTertiaryContainer
+val md_theme_light_error = md_theme_dark_error
+val md_theme_light_errorContainer = md_theme_dark_errorContainer
+val md_theme_light_onError = md_theme_dark_onError
+val md_theme_light_onErrorContainer = md_theme_dark_onErrorContainer
+val md_theme_light_background = md_theme_dark_background
+val md_theme_light_onBackground = md_theme_dark_onBackground
+val md_theme_light_surface = md_theme_dark_surface
+val md_theme_light_onSurface = md_theme_dark_onSurface
+val md_theme_light_surfaceVariant = md_theme_dark_surfaceVariant
+val md_theme_light_onSurfaceVariant = md_theme_dark_onSurfaceVariant
+val md_theme_light_outline = md_theme_dark_outline
+val md_theme_light_inverseOnSurface = md_theme_dark_inverseOnSurface
+val md_theme_light_inverseSurface = md_theme_dark_inverseSurface
+val md_theme_light_inversePrimary = md_theme_dark_inversePrimary
+val md_theme_light_shadow = md_theme_dark_shadow
+val md_theme_light_surfaceTint = md_theme_dark_surfaceTint
+val md_theme_light_outlineVariant = md_theme_dark_outlineVariant
+val md_theme_light_scrim = md_theme_dark_scrim
 
 
 val seed = Color(0xFF2FAFDD)
